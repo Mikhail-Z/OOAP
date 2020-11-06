3b6f96b782af75a12c279916c036f403199218a0
Забелин Михаил Константинович <zabelin.mihail@wildberries.ru>
2020-11-07 00:17:23 +0300

    сделано задание 18

---
+++ OOAP2/Tasks/src/ru/skillsmart/ooap2/task18/task18.java
@@ -0,0 +1,61 @@
+package ru.skillsmart.ooap2.task18;
+
+class Scales {
+
+}
+
+class LaboratoryScales extends Scales {
+
+}
+
+class CommercialScales extends Scales {
+
+}
+
+class Rectangle {
+    protected int length;
+    protected int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+}
+
+class Square extends Rectangle {
+    public Square(int length) {
+        super(length, length);
+    }
+}
+
+class SurfingBoard {
+    void surfByWaves() {
+        System.out.println("surfing by wind...");
+    }
+}
+
+class JetBoard extends SurfingBoard {
+    void surfByElectricEngine() {
+        System.out.println("surfing by wind...");
+    }
+}
+
+
+public class task18 {
+    public static void main(String[] args) {
+        task18();
+    }
+
+    //Задание 18.
+    //Приведите словесные примеры случаев наследования подтипов, с ограничением и с расширением.
+    static void task18() {
+        //Наследование подтипов -- примером является отношение между абстрактными весами и различными типами весов по их применению, например (лабораторные, товарные и т.д.)
+        // Scales -- LaboratoryScales, CommercialScales
+
+        //наследование с ограничением -- примером является отношение между прямоугольником и квадратом (важным частным случаем прямоугольника).
+        //Rectangle -- Square
+
+        //наследование с раширением -- примером является иерархия обычной доски для серфинга и доски для серфинга с двигателем,
+        // позволяющим передвигаться по водной глади без волн (SurfingBoard -- JetBoard)
+    }
+}
